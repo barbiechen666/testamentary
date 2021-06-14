22cd1d97b6bb99712cd0dcccc06ca006d9361abc
barbiechen666 <44830698+barbiechen666@users.noreply.github.com>
2021-06-14 12:35:09 +0800

    Update testamentary.sol

--- testamentary.sol
+++ testamentary.sol
@@ -15,18 +15,23 @@ contract Bank{
     }*/
     //mapping(uint=>Beneficiary) beneficiaryinfo;
     //uint[] public beneficiaryids;
+    event Deposit(address user,uint amount);
+    event Withdraw(address user,uint amount);
     
     //存錢進合約
     function deposit() public payable{
         balances[msg.sender]+=msg.value;
+        emit Deposit(msg.sender,msg.value);
     }
     //領錢出來
     function withdraw(uint amount) payable public{
         if(balances[msg.sender]>=amount){
             balances[msg.sender]-=amount;
-            msg.sender.transfer(amount*1000000000000000000);
+            msg.sender.transfer(amount);
+            emit Withdraw(msg.sender,amount);
         }
     }
+    
     //查看合約的錢
     function getBankBalance() public view returns(uint){
         return this.balance;
@@ -64,6 +69,9 @@ contract Bank{
     }*/
 
     //簽立遺囑人信箱
+    /*function submitEmail(string memory _email) public {
+       owneremail = _email;
+    }*/
     function submitEmail(string memory _email) public {
        owneremail = _email;
     }
@@ -100,9 +108,11 @@ contract Bank{
         return portions[_mail];
     }
     //修改受益人分配比例
-    function modifybene(uint _id,uint _portion)public{
+    function modifybene(uint _id,string memory _mail,uint _portion)public{
         Beneficiary storage  s =beneficiaryinfo[_id];
         s.portion=_portion;
+        portions[_mail]=_portion;
+        
         
     }
     function submitTransaction(address _to,uint _portion) payable public {
@@ -119,6 +129,8 @@ contract setpassword{
      uint hashmoduls=10**hashdigits;
      mapping(address=>uint) beneficiarypass;
      mapping(uint=>uint) beneficiaryportion;
+     mapping(string=>uint)portions;
+     mapping(address=>string)mail;
      
      function setpassword(address _contractadd) public{
         bank=Bank(_contractadd);
@@ -130,73 +142,38 @@ contract setpassword{
         else{i="invalid";}
         return i;
      }*/
-     function passnportionset(string memory _mail,string memory _password) public{
+     function passset(string memory _mail,string memory _password) public{
          //password=uint((keccak256(abi.encodePacked(_password)))%hashmoduls);
          require(bank.getbeneficiarybymail(_mail)==true);
          password=uint((keccak256(_password)));
          //password=_password;
          beneficiarypass[msg.sender]=password;
-         beneficiaryportion[password]=bank.getportion(_mail);
+         mail[msg.sender]=_mail;
+         //beneficiaryportion[password]=bank.getportion(_mail);
+     }
+     function getmail()public view returns(string memory){
+         return mail[msg.sender];
      }
 
      function getpassword() public view returns(uint){
          return beneficiarypass[msg.sender];
      }
-     function getportion(string memory _password) public view returns(uint) {
+     /*function getportion(string memory _password) public view returns(uint) {
          password=uint((keccak256(_password)));
          return beneficiaryportion[password];
          
-     }
+     }*/
+    function getmypo()public view returns(uint){
+        return bank.getportion(mail[msg.sender]);
+    }
 
     function execute(string memory _password)public payable{
          require(uint(keccak256(_password))==beneficiarypass[msg.sender]);
          //portion=beneficiaryportion[uint(keccak256(_password))];
-         portion=getportion(_password);
+         portion=bank.getportion(mail[msg.sender]);
          bank.submitTransaction(msg.sender,portion);
          //msg.sender.transfer(bank.getBankBalance()/100*beneficiaryportion[uint(keccak256(_password))]);
      }
 
 
 }
-    /*function execution(string memory _email)public{
-        Beneficiary storage s = beneficiaryinfo[id];
-        require(s.execute=false);
-    }*/
-    
-    //受益人信箱
-    /*function submitbeneficiaryEmail(string memory _benemail) public {
-       beneficiaryemail = _benemail;
-       beneemail.push(beneficiaryemail);
-       
-    }*/
-    /*function getbeneficiaryEmail(uint index) public view returns (string memory) {
-        //return beneficiaryemail;
-        return beneemail[index];
-    }*/
-    /*function setportion(uint _distriburate) public{
-        portion=_distriburate;
-        portionli.push(portion);
-        
-    }
-    function getportion(uint index) public view returns (uint){
-        return portionli[index];
-    }*/
-    //function getlen() public view returns(uint){
-        //return beneemail.length;
-    //}
-    /*function uintToString(uint v) constant returns (string str) {
-        uint maxlength = 100;
-        bytes memory reversed = new bytes(maxlength);
-        uint i = 0;
-        while (v != 0) {
-            uint remainder = v % 10;
-            v = v / 10;
-            reversed[i++] = byte(48 + remainder);
-        }
-        bytes memory s = new bytes(i + 1);
-        for (uint j = 0; j <= i; j++) {
-            s[j] = reversed[i - j];
-        }
-        str = string(s);
-    }*/
-//}
